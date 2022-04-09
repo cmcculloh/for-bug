1dcf55331249482b4c30c34772400768c9f2c9bc
Christopher McCulloh <cmcculloh@salesforce.com>
2022-04-08 20:57:45 -0400

    working example of it breaking

---
+++ src/lib/foo.ts
@@ -0,0 +1,7 @@
+import { Banners } from 'working-json/bar.js';
+
+const generate = () => {
+	return Banners.generate();
+};
+
+export { generate };
---
+++ src/routes/index.svelte
@@ -0,0 +1,6 @@
+<script>
+import { generate } from '$lib/foo';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>{generate()}</p>
